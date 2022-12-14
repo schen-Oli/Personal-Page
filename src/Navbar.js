10219e09476edcabf66b63f687603a210e4f9d04
Oli Chen <schen@Olis-MacBook-Air.local>
2022-12-13 16:25:36 -0800

    add navbar

---
+++ src/Navbar.js
@@ -0,0 +1,17 @@
+import './Navbar.css'
+
+const Navbar = () => {
+    return ( 
+        <nav className="navbar">
+            <h1>Oli Chen</h1>
+            <div className="links">
+                <a href="/">Home</a>
+                <a href="/experiences">Experiences</a>
+                <a href="/projects">Projects</a>
+                <a href="/contact">Contact</a>
+            </div>
+        </nav>
+    );
+}
+ 
+export default Navbar;
