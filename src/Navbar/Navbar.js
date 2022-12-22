2ea9bfa6be82b91adfa118e9f5172867de576635
Oli Chen <olichen97@gmail.com>
2022-12-22 03:28:42 -0800

    make navbar responsive

--- src/Navbar/Navbar.js
+++ src/Navbar/Navbar.js
@@ -1,30 +1,42 @@
 import './Navbar.css'
 import { NavLink } from 'react-router-dom';
-const Navbar = () => {
+import { Component } from 'react';
 
-    let activeClassName = "activeLink";
-    return (
-        <nav className="navbar">
-            <h1><a href="/">Oli Chen</a></h1>
-            <div className="links">
-                <NavLink to="/" exact={true} className={({ isActive }) =>
-                    isActive ? activeClassName : undefined
-                }>Home</NavLink>
-                <NavLink to="/education" className={({ isActive }) =>
-                    isActive ? activeClassName : undefined
-                }>Education</NavLink>
-                <NavLink to="/experiences" className={({ isActive }) =>
-                    isActive ? activeClassName : undefined
-                }>Experiences</NavLink>
-                <NavLink to="/projects" className={({ isActive }) =>
-                    isActive ? activeClassName : undefined
-                }>Projects</NavLink>
-                <NavLink to="/contact" className={({ isActive }) =>
-                    isActive ? activeClassName : undefined
-                }>Contact</NavLink>
-            </div>
-        </nav>
-    );
+
+class Navbar extends Component {
+
+    activeClassName = "activeLink";
+    state = { clicked: false };
+
+    handleClick = ()=>{
+        this.setState({clicked : !this.state.clicked})
+    }
+
+    render() {
+        return (
+            <nav className="navbar">
+                <h1><a href="/">Oli Chen</a></h1>
+                <div className={ this.state.clicked ? "links active" : "links"}>
+                    <NavLink to="/" exact={true} className={({ isActive }) =>
+                        isActive ? this.activeClassName : undefined
+                    }>Home</NavLink>
+                    <NavLink to="/education" className={({ isActive }) =>
+                        isActive ? this.activeClassName : undefined
+                    }>Education</NavLink>
+                    <NavLink to="/experiences" className={({ isActive }) =>
+                        isActive ? this.activeClassName : undefined
+                    }>Experiences</NavLink>
+                    <NavLink to="/projects" className={({ isActive }) =>
+                        isActive ? this.activeClassName : undefined
+                    }>Projects</NavLink>
+                    <NavLink to="/contact" className={({ isActive }) =>
+                        isActive ? this.activeClassName : undefined
+                    }>Contact</NavLink>
+                </div>
+                <button onClick={this.handleClick} className="burger"><ion-icon name={this.state.clicked ? "close" : "menu"}></ion-icon></button>
+            </nav>
+        );
+    }
 }
 
 export default Navbar;
